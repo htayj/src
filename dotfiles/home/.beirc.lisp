17760dfeba6a89b804c9428694d778ee2ff3b773
htayj <htayj@users.noreply.github.com>
2026-05-10 05:12:17 -0400

    Add BeIRC startup config

---
+++ dotfiles/home/.beirc.lisp
@@ -0,0 +1,9 @@
+(in-package :beirc)
+
+(setf *default-nick* "htayjbeirc")
+
+(setf *auto-connect-list*
+      '("irc.libera.chat"))
+
+(setf *auto-join-alist*
+      '(("irc.libera.chat" . ("#commonlisp" "#lispgames" "##emacs"))))
